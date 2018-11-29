6183ef615815263666a30547e73a3f977d58412d
hchX004 <hecenhao@outlook.com>
2018-11-29 11:35:24 +0800

    201811291135 commit

--- js/point_child.js
+++ js/point_child.js
@@ -2,10 +2,14 @@ $(document).ready(function($) {
 
 	var url = window.location.search.substr(1);
 	var params = decodeURIComponent(url).split('&');
+	var num = 300;
+	var newsText = new Array(num + 1).join("这是内容");
 
 	var pic_src = params[0].split("=")[1];
 	var title = params[1].split("=")[1];
 	var text = params[2].split("=")[1];
+
+	text = text + newsText + "。";
 
 	$('img').attr('src', pic_src);
 	$('h1').text(title);
